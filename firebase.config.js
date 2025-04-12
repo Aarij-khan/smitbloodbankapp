3f9694ec1c35401203afe8c8f14ffa5d33571c2b
Aarij khan <aarij0100@gmail.com>
2025-04-13 00:04:28 +0500

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -4,12 +4,7 @@ import { getAuth ,getReactNativePersistence, initializeAuth } from "firebase/aut
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUb3xlWf0Acpj6Wvs12eTfpZ1TSwrsmTw",
-  authDomain: "expo-project-727f1.firebaseapp.com",
-  projectId: "expo-project-727f1",
-  storageBucket: "expo-project-727f1.appspot.com",
-  messagingSenderId: "455427492250",
-  appId: "1:455427492250:web:e6532133bccd238cc7d223"
+
 };
 
 const app = initializeApp(firebaseConfig);
